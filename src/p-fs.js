91cb92b0d487737a6bff540f41b2ce6fe72046b9
vincentzheng <zhs0518@qq.com>
2015-10-03 23:55:24 +0800

    test a new way to get the api

--- src/p-fs.js
+++ src/p-fs.js
@@ -3,13 +3,55 @@ var fs = require('fs')
 var p_fs = {}
 
 var ErrorStringArr = {
-	FileNameNotString = 'The file name must be a string!!!',
-	DataNotString = 'The data must be a string!!!'
+	FileNameNotString: 'The file name must be a string!!!',
+	DataNotString: 'The data must be a string!!!'
 }
 
 var syncApi = [
+	'renameSync',
+	'ftruncateSync',
+	'truncateSync',
 	'readFileSync',
 	'writeFileSync'
+]
+
+var asyncApi = [
+	'rename',
+	'truncate',
+	'ftruncate',
+	'chown',
+	'fchown',
+	'lchown',
+	'chmod',
+	'fchmod',
+	'lchmod',
+	'stat',
+	'lstat',
+	'fstat',
+	'link',
+	'symlink',
+	'realpath',
+	'unlink',
+	'rmdir',
+	'mkdir',
+	'readdir',
+	'close',
+	'open',
+	'utimes',
+	'futimes',
+	'fsync',
+	'appendFile',
+	//@TODO: unknown
+	'watchFile',
+	'unwatchFile',
+	
+	
+	'exists',
+	
+	
+	'access',
+	'readFile',
+	'writeFile'
 ]
 
 // fecth the sync api
@@ -17,43 +59,25 @@ syncApi.forEach(function (functionName) {
 	p_fs[functionName] = fs[functionName]
 })
 
-p_fs.readFile = function (fileName, _encoding) {
-	var encoding = _encoding || 'utf-8'
-	return new Promise(function (resolve, reject) {
-		if (typeof fileName !== 'string') {
-			reject(new Error(ErrorStringArr.FileNameNotString))
-			return
-		}
-		fs.readFile(fileName, encoding, function (err, data) {
-			if (err) {
-				reject(err)
-			} else {
+
+// fecth the async api
+asyncApi.forEach(function (funcName) {
+	p_fs[funcName] = function () {
+		var argc = fs[funcName].length
+		var argv = Array.from(arguments).slice(0, argc);		
+		return new Promise(function (resolve, reject) {
+			argv[argc] = function (err, data) {
+				if (err) {
+					reject(err)
+					return 
+				}
 				resolve(data)
 			}
+			
+			fs[funcName].apply(null, argv)
 		})
-	})
-}
-
-p_fs.writeFile = function (fileName, data, _options) {
-	var options = _options || {}
-	return new Promise(function (resolve, reject) {
-		if (typeof fileName !== 'string') {
-			reject(new Error(ErrorStringArr.FileNameNotString))
-			return 
-		}
-		if (typeof data !== 'string') {
-			reject(new Error(ErrorStringArr.DataNotString))
-			return 
-		}
-		fs.writeFile(fileName, data, options, function (err) {
-			if (err) {
-				reject(err)
-			} else {
-				resolve()
-			}
-		})
-	})
-}
+	}
+})
 
 
 module.exports = p_fs
